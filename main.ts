468a485fedfc2440e11eef44bb2ce8f7eb7b5c51
k-kid <76645937+k-kid@users.noreply.github.com>
2020-12-27 20:33:51 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    Bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Bird.change(LedSpriteProperty.Y, 1)
+})
+let Bird: game.LedSprite = null
+let Obstacles: game.LedSprite[] = []
+Bird = game.createSprite(0, 2)
+Bird.set(LedSpriteProperty.Blink, 300)
+let Empty_ObstacleY = randint(0, 4)
+for (let index = 0; index <= 4; index++) {
+    if (index != Empty_ObstacleY) {
+        Obstacles.push(game.createSprite(4, index))
+    }
+}
+basic.forever(function () {
+    while (Obstacles.length > 0 && Obstacles[0].get(LedSpriteProperty.X) == "0") {
+        Obstacles.removeAt(0).delete()
+    }
+    for (let Obstacle of Obstacles) {
+        Obstacle.change(LedSpriteProperty.X, -1)
+    }
+    basic.pause(1000)
+})
